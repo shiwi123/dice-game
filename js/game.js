b7983056e0b9b27cb6874dcc68ec75c885185dd6
shiwani jha <shiwanikumari835@gmail.com>
2021-06-05 18:49:54 +0530

    first commit

---
+++ js/game.js
@@ -0,0 +1,41 @@
+
+    // Player name
+    var player1 = "Player 1";
+    var player2 = "Player 2";
+  
+    // Function to change name
+    function edit() {
+        player1 = prompt("Change Player1 name");
+        player2 = prompt("Change player2 name");
+  
+        document.querySelector("p.Play1").innerHTML = player1;
+        document.querySelector("p.Play2").innerHTML = player2;
+    }
+  
+    // Function to roll the dice
+    function rollDice() {
+        setTimeout(function () {
+            var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+            var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  
+            document.querySelector(".img1").setAttribute("src",
+                "/images/dice" + randomNumber1 + ".png");
+  
+            document.querySelector(".img2").setAttribute("src",
+                "/images/dice" + randomNumber2 + ".png");
+  
+            if (randomNumber1 === randomNumber2) {
+                document.querySelector("h1").innerHTML = "Draw!";
+            }
+  
+            else if (randomNumber1 < randomNumber2) {
+                document.querySelector("h1").innerHTML
+                                = (player2 + " WINS!");
+            }
+  
+            else {
+                document.querySelector("h1").innerHTML
+                                = (player1 + " WINS!");
+            }
+        }, 2500);
+    }
